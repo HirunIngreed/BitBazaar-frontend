b26c2becb188324d402221e2dc3787312abb0d7f
Hirun <hingreed738@gmail.com>
2026-01-02 23:03:26 +0530

    Aath cliant id determination 🖋️

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
 
   return (
-    <GoogleOAuthProvider clientId="http://202783816034-9to29ngs05ljfp5hk0cjn88c5jtbotri.apps.googleusercontent.com">
+    <GoogleOAuthProvider clientId="202783816034-9to29ngs05ljfp5hk0cjn88c5jtbotri.apps.googleusercontent.com">
    <BrowserRouter>
    <Toaster position="top-center"/>
       <div className="w-full h-screen">
